1d1c826f1abac05b7bbc963f72dc9e2ec288d3a6
Sindre Bøyum <sindre.boyum@gmail.com>
2021-06-29 11:28:51 +0200

    null check interaction label settings

--- scripts/components/Scene/SceneTypes/StaticScene.js
+++ scripts/components/Scene/SceneTypes/StaticScene.js
@@ -423,7 +423,7 @@ export default class StaticScene extends React.Component {
               const key = interaction.id || `interaction-${this.props.sceneId}${index}`
 
               return (
-                interaction.label.showAsOpenSceneContent ?
+                interaction?.label.showAsOpenSceneContent ?
                 <OpenContent
                   key={key}
                   staticScene={true}
